305244da607ebddf254364b57438b64f19c976c2
Altanim <altanim0404@gmail.com>
2022-11-02 15:46:10 +0800

    Выполнил задачи 1, 2 и 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Задача №1:
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+
+        // Задача №2:
+        dog += 4;
+        System.out.println(dog);
+        cat += 4;
+        System.out.println(cat);
+        paper += 4;
+        System.out.println(paper);
+
+        // Задача №3:
+        dog -= 3.5; // изменил значение dog на 8.0
+        System.out.println(dog);
+        cat -= 1.6;
+        System.out.println(cat);
+        paper -= 7639;
+        System.out.println(paper);
+
+
     }
 }
